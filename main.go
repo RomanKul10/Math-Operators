060d45e64c46970499f584276222e2647dc21ad7
Roman Kulkov <romawka2410@gmail.com>
2023-11-29 17:39:18 +0200

    relational and conditonal operators

--- main.go
+++ main.go
@@ -1,25 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 
-	/* //modulus
-	// does one number divide EXACLY INTO ANOTHER?
-	x := 12
-	y := 3
-	if x%y == 0 {
-		fmt.Println(y, "divides execly into", x)
+	/* //relational and conditonal operators - відносні та умовні оператори
+
+	second := 31
+	minute := 1
+
+	if minute < 59 && second+1 > 59 {
+		minute++
+	}
+	fmt.Println(minute) */
+
+	a := 12
+	b := 3
+
+	/* 	if b != 0 {
+		c := divideTwoNumbers(a, b)
+
+		if c == 2 {
+			fmt.Println("We found a two")
+		}
+	} */
+	/* if b != 0 && divideTwoNumbers(a, b) == 2 {
+		fmt.Println("Found 2")
+	} */
+
+	// this case never work
+	/* if divideTwoNumbers(a, b) == 2 && b != 0 {
+		fmt.Println("Found 2")
+	} */
+	c, err := divideTwoNumbers(a, b)
+	if err != nil {
+		fmt.Println(err)
 	} else {
-		fmt.Println(y, "does not divide execly into", x)
+		if c == 2 {
+			fmt.Println("We found 2")
+		}
 	}
 
-	thisMonth := 4
-	fmt.Println("The next month after ", thisMonth, "is", thisMonth+1) */
+}
 
-	for m := 1; m <= 12; m++ {
-		fmt.Println("The month after", m, "is", m%12+1)
+/* func divideTwoNumbers(x, y int) int {
+return x / y */
 
+func divideTwoNumbers(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divide by 0")
 	}
-
+	return x / y, nil
 }
